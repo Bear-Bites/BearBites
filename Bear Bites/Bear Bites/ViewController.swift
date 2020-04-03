bcd63105a55a48d5f3632257bcd59e85ab34b482
George Martin <georgeg10live@gmail.com>
2020-04-03 08:44:24 -0400

    created new xcode project for bear bites

---
+++ Bear Bites/Bear Bites/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bear Bites
+//
+//  Created by George Martin on 4/3/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
