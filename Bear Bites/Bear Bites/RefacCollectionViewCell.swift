70916a8872101654cfcf2e13abe5f1c2f83daa70
George Martin <georgeg10live@gmail.com>
2020-04-12 18:35:33 -0400

    adding json file for rawlings menu

---
+++ Bear Bites/Bear Bites/RefacCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RefacCollectionViewCell.swift
+//  Bear Bites
+//
+//  Created by George Martin on 4/12/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+
+class RefacCollectionViewCell: UICollectionViewCell {
+    
+}
