0055b2b8bf1a9ed118566f3957dcbfa975d5baa8
elwin <readyelwin@gmail.com>
2020-04-12 21:19:44 -0400

    Added Outlets for FoodDetailViewController

--- Bear Bites/Bear Bites/FoodDetailViewController.swift
+++ Bear Bites/Bear Bites/FoodDetailViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class FoodDetailViewController: UIViewController {
 
+    @IBOutlet weak var foodImage: UIImageView!
+    
+    @IBOutlet weak var foodLabel: UILabel!
+    
+    
+    @IBOutlet weak var foodType1: UILabel!
+    
+    @IBOutlet weak var foodType2: UILabel!
+    
+    @IBOutlet weak var foodType3: UILabel!
+    
+    @IBOutlet weak var reviewLabel: UILabel!
+    
+    @IBOutlet weak var reviewBox: UITextView!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var reviewStars: UIImageView!
+    
+    @IBOutlet weak var commentView: UITableView!
+    @IBOutlet weak var foodRating: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
