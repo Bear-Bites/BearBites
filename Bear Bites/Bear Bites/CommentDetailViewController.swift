676acdcfcb356787069819922ea77cb28bd73e21
Dexsidius <dashawnlarry@gmail.com>
2020-04-18 19:59:01 -0400

    Created CommentDetailViewController class

---
+++ Bear Bites/Bear Bites/CommentDetailViewController.swift
@@ -0,0 +1,49 @@
+//
+//  CommentDetailViewController.swift
+//  Bear Bites
+//
+//  Created by user168008 on 4/18/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+
+class CommentDetailV_iewController: UIViewController {
+    
+    
+    @IBAction func newestSort(_ sender: Any) {
+    }
+    
+    @IBAction func mostPopularSort(_ sender: Any) {
+    }
+    
+    @IBAction func mostPositiveSort(_ sender: Any) {
+    }
+    
+    @IBAction func mostCriticalSort(_ sender: Any) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        // Do any additional setupafter loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
