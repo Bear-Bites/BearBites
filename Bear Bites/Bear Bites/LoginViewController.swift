df39c16ac21b0541a62f9b48c7adf1dea13d977e
George Martin <georgeg10live@gmail.com>
2020-04-19 08:46:12 -0400

    created login screen skeleton and downloaded neccasaryy pods for it

---
+++ Bear Bites/Bear Bites/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  Bear Bites
+//
+//  Created by George Martin on 4/19/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
