a1e09c07ab584e85ece705abdd48fa5ef746f672
Dexsidius <dashawnlarry@gmail.com>
2020-04-24 00:20:57 -0400

    Started code for CommentView.swift

--- Bear Bites/Bear Bites/CommentDetailViewController.swift
+++ Bear Bites/Bear Bites/CommentDetailViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import Parse
+
 
 class CommentDetailV_iewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var postCell: UITableViewCell!
+    
+   
     @IBOutlet weak var authorName: UILabel!
     
     @IBOutlet weak var authorComment: UILabel!
@@ -21,11 +28,18 @@ class CommentDetailV_iewController: UIViewController {
     @IBAction func filterControl(_ sender: Any) {
     }
     
+    var comments = [PFObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        
+    func tableView(_ tableView:UITableView,numberOfRowsInSection section: Int)->Int{
+            return 0
+    }
+        func tableView(_ tableView:UITableView,cellForRowAt indexPath:IndexPath)->UITableViewCell{
+            
+        }
+    
         
         
         // Do any additional setupafter loading the view.
