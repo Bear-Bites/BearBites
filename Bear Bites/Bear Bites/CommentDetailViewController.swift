7b5c467902c9420ebaa408688156f36319966cea
Elwin <readyelwin@gmail.com>
2020-04-28 17:37:26 -0400

    Started to redo Comment DEtailViewController

---
+++ Bear Bites/Bear Bites/CommentDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CommentDetailViewController.swift
+//  Bear Bites
+//
+//  Created by user167669 on 4/28/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+
+class CommentDetailViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
