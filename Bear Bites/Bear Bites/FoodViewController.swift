c3142cf3a7f29793de9636d55839aabe61de7f78
George Martin <georgeg10live@gmail.com>
2020-05-03 21:32:57 -0400

    show top reviewa

--- Bear Bites/Bear Bites/FoodViewController.swift
+++ Bear Bites/Bear Bites/FoodViewController.swift
@@ -10,24 +10,31 @@ import UIKit
 import Parse
 import AlamofireImage
 
-class FoodViewController: UIViewController {
+class FoodViewController: UIViewController,  UITableViewDataSource, UITableViewDelegate{
+    
     
     @IBOutlet weak var foodImage: UIImageView!
-        @IBOutlet weak var foodLabel: UILabel!
-        @IBOutlet weak var veganLabel: UILabel!
-        @IBOutlet weak var dairyLabel: UILabel!
-        @IBOutlet weak var vegetarianLabel: UILabel!
-        @IBOutlet weak var reviewCount: UILabel!
-        @IBOutlet weak var caloriesLabel: UILabel!
-        @IBOutlet weak var foodRating: UIImageView!
-        
-        
-        var menu_item: PFObject!
-        var reviews = [PFObject]()
-        var review: PFObject!
-        
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var veganLabel: UILabel!
+    @IBOutlet weak var dairyLabel: UILabel!
+    @IBOutlet weak var vegetarianLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var foodRating: UIImageView!
+    
+    
+    var menu_item: PFObject!
+    var reviews = [PFObject]()
+    var review: PFObject!
+    var reviewCount: Int!
+    @IBOutlet weak var tableView: UITableView!
+    
         override func viewDidLoad() {
             super.viewDidLoad()
+            super.viewDidLoad()
+            tableView.delegate = self
+            tableView.dataSource = self
+            
             let imagePath = menu_item["image"] as! String
             let foodUrl = URL(string: imagePath)
             foodImage.af_setImage(withURL: foodUrl!)
@@ -46,13 +53,33 @@ class FoodViewController: UIViewController {
             query.includeKeys(["author", "body", "menuItem", "likes", "dislikes", "date"])
             query.whereKey("menuItem", equalTo: menu_item)
             
+            
+            
+            query.limit = 2
             query.findObjectsInBackground {(reviews, error) in
                 if reviews != nil {
                     self.reviews = reviews!
+                    self.tableView.reloadData()
                 }
             }
-            print("reviews", reviews.count)
+            print("reviews", reviewCount)
         }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reviews.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopReviewCell") as! TopReviewsCell
+        
+        let review = reviews[indexPath.row]
+        
+        let user = review["author"] as! PFUser
+        cell.userNameLabel.text = user.username
+        cell.reviewBody.text = review["body"] as? String
+        
+        
+        return cell
+    }
         
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             // Get the new view controller using segue.destination.
---
+++ Bear Bites/Bear Bites/TopReviewsCell.swift
@@ -0,0 +1,26 @@
+//
+//  TopReviewsCell.swift
+//  Bear Bites
+//
+//  Created by George Martin on 5/3/20.
+//  Copyright © 2020 Bear-Bites. All rights reserved.
+//
+
+import UIKit
+
+class TopReviewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var reviewBody: UILabel!
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
